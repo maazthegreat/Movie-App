2cef715127c82895b08cc0fc385782bd63446df6
maazthegreat <127127699+maazthegreat@users.noreply.github.com>
2023-11-14 12:47:48 +0500

    Add files via upload

---
+++ build/precache-manifest.bbd145a5f75e6cd5b5a7b9d0cce2b31e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de542a5de29298fe6c8ec8f251869c18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de6e64d34123bc5dd73b",
+    "url": "/static/css/2.1ba447cc.chunk.css"
+  },
+  {
+    "revision": "3e693230f9eefdc5fc0b",
+    "url": "/static/css/main.7be2f6f0.chunk.css"
+  },
+  {
+    "revision": "de6e64d34123bc5dd73b",
+    "url": "/static/js/2.b5e741b4.chunk.js"
+  },
+  {
+    "revision": "3e693230f9eefdc5fc0b",
+    "url": "/static/js/main.b73db937.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
